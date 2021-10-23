818084fd878678fcbb193563be7682c4cf282900
Michal Jankowski <michaljankowskireal@gmail.com>
2021-10-23 16:41:15 +0200

    Update setupWindow with appropriate sync and game timer

--- src/initWindow.cpp
+++ src/initWindow.cpp
@@ -1,45 +1,18 @@
 #include <iostream>
-#include <tuple>
-#include <GLAD/glad.h>
-#include <GLFW/glfw3.h>
+#include "../src/GameScene.h"
+#include "../src/Shader.h"
+#include "../src/ShaderProgram.h"
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-void processInput(GLFWwindow* window);
-auto WINDOW_DIMENSION = std::make_tuple(800, 600);
 int main(void) {
-	glfwInit();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	GLFWwindow* window = glfwCreateWindow(std::get<0>(WINDOW_DIMENSION), std::get<1>(WINDOW_DIMENSION), "SpaceShooter", NULL, NULL);
-	if (window == NULL) {
-		std::cout << "Failed to create GLFW window" << std::endl;
-		glfwTerminate();
-		return -1;
+	const std::string& windowTitle = "SpaceShooter";
+	int majorVersion = 4;
+	int minorVersion = 4;
+	GameScene gameScene;
+	if (!gameScene.createWindow(windowTitle, majorVersion, minorVersion, false))
+	{
+		printf("Failed to create window with OpenGL context %d.%d! Shutting down...\n", majorVersion, minorVersion);
+		return 1;
 	}
-	glfwMakeContextCurrent(window);
-	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-		std::cout << "Failed to initialize GLAD" << std::endl;
-		return -1;
-	}
-	while (!glfwWindowShouldClose(window)) {
-		processInput(window);
-		glClearColor(0.5f, 0.0f, 0.5f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
-		glfwSwapBuffers(window);
-		glfwPollEvents();
-	}
-	glfwTerminate();
-	return 0;
-}
+	gameScene.runApp();
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
-	glViewport(0, 0, width, height);
-}
-
-void processInput(GLFWwindow* window) {
-	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
-		glfwSetWindowShouldClose(window, true);
-	}
 }
