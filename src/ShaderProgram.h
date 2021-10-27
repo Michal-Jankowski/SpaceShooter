20b8873172f3c5d5e19e924846696e74014b62ea
Michal Jankowski <michaljankowskireal@gmail.com>
2021-10-27 19:05:49 +0200

    Change plain strings to const ref ones

--- src/ShaderProgram.h
+++ src/ShaderProgram.h
@@ -17,31 +17,31 @@ public:
 	GLuint getProgramID() const;
 
 	// Setting vectors
-	void setUniform(std::string sName, glm::vec2* vVectors, int iCount = 1);
-	void setUniform(std::string sName, const glm::vec2 vVector);
-	void setUniform(std::string sName, glm::vec3* vVectors, int iCount = 1);
-	void setUniform(std::string sName, const glm::vec3 vVector);
-	void setUniform(std::string sName, glm::vec4* vVectors, int iCount = 1);
-	void setUniform(std::string sName, const glm::vec4 vVector);
+	void setUniform(const std::string& sName, glm::vec2* vVectors, int iCount = 1);
+	void setUniform(const std::string& sName, const glm::vec2 vVector);
+	void setUniform(const std::string& sName, glm::vec3* vVectors, int iCount = 1);
+	void setUniform(const std::string& sName, const glm::vec3 vVector);
+	void setUniform(const std::string& sName, glm::vec4* vVectors, int iCount = 1);
+	void setUniform(const std::string& sName, const glm::vec4 vVector);
 
 	// Setting floats
-	void setUniform(std::string sName, float* fValues, int iCount = 1);
-	void setUniform(std::string sName, const float fValue);
+	void setUniform(const std::string& sName, float* fValues, int iCount = 1);
+	void setUniform(const std::string& sName, const float fValue);
 
 	// Setting 3x3 matrices
-	void setUniform(std::string sName, glm::mat3* mMatrices, int iCount = 1);
-	void setUniform(std::string sName, const glm::mat3 mMatrix);
+	void setUniform(const std::string& sName, glm::mat3* mMatrices, int iCount = 1);
+	void setUniform(const std::string& sName, const glm::mat3 mMatrix);
 
 	// Setting 4x4 matrices
-	void setUniform(std::string sName, glm::mat4* mMatrices, int iCount = 1);
-	void setUniform(std::string sName, const glm::mat4 mMatrix);
+	void setUniform(const std::string&  sName, glm::mat4* mMatrices, int iCount = 1);
+	void setUniform(const std::string&  sName, const glm::mat4 mMatrix);
 
 	// Setting integers
-	void setUniform(std::string sName, int* iValues, int iCount = 1);
-	void setUniform(std::string sName, const int iValue);
+	void setUniform(const std::string&  sName, const int* iValues, int iCount = 1);
+	void setUniform(const std::string&  sName, const int iValue);
 
 	// Model and normal matrix setting
-	void SetModelAndNormalMatrix(std::string sModelMatrixName, std::string sNormalMatrixName, glm::mat4 mModelMatrix);
+	void SetModelAndNormalMatrix(const std::string& sModelMatrixName, const std::string& sNormalMatrixName, glm::mat4 mModelMatrix);
 
 private:
 	GLuint m_programID;
