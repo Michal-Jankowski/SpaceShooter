f155a23457ada5a4b715d4d0083fb7e155f5e1eb
Michal Jankowski <michaljankowskireal@gmail.com>
2021-10-27 19:03:57 +0200

    Fix typo in  main fs shader

--- src/shaders/shader.fs
+++ src/shaders/shader.fs
@@ -5,10 +5,10 @@ layout(location = 0) out vec4 outputColour;
 smooth in vec2 verTexCoord;
 
 uniform sampler2D sampler;
-uniform vec4 colour;
+uniform vec4 color;
 
 void main()
 {
     vec4 texColor = texture(sampler, verTexCoord);
-    outputColour = texColor * colour;
+    outputColour = texColor * color;
 }
