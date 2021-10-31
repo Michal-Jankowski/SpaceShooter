170880dcbd249914078054e0af46c5ed967e0673
Michal Jankowski <michaljankowskireal@gmail.com>
2021-10-31 18:54:21 +0100

    Refactor of code

--- src/Skybox.cpp
+++ src/Skybox.cpp
@@ -1,1 +1,13 @@
 #include "Skybox.h"
+
+Skybox::Skybox(const std::string& path) {
+}
+
+Skybox::~Skybox() {
+}
+
+void Skybox::render(const glm::vec3& position, ShaderProgram& shader) const  {
+}
+
+void Skybox::loadTexture(const int sideBit) const {
+}
