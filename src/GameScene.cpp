911c2b0451d92840ad3f936e1353be2b5c50a003
Michal Jankowski <michaljankowskiReal@gmail.com>
2021-10-31 21:59:50 +0100

    Add source files to debug and release dirs with appropriate file paths

--- src/GameScene.cpp
+++ src/GameScene.cpp
@@ -41,10 +41,10 @@ glm::vec2 quad2D[] =
 void GameScene::initScene() {
 	glClearColor(0.2, 0.7f, 0.2f, 1.0f);
 	// load shaders
-	m_vsShader.loadShaderFromFile("../src/shaders/shader.vs", GL_VERTEX_SHADER);
-	m_fsShader.loadShaderFromFile("../src/shaders/shader.fs", GL_FRAGMENT_SHADER);
-	m_vsGround.loadShaderFromFile("../src/shaders/ground.vs", GL_VERTEX_SHADER);
-	m_fsGround.loadShaderFromFile("../src/shaders/ground.fs", GL_FRAGMENT_SHADER);
+	m_vsShader.loadShaderFromFile("src/shaders/shader.vs", GL_VERTEX_SHADER);
+	m_fsShader.loadShaderFromFile("src/shaders/shader.fs", GL_FRAGMENT_SHADER);
+	m_vsGround.loadShaderFromFile("src/shaders/ground.vs", GL_VERTEX_SHADER);
+	m_fsGround.loadShaderFromFile("src/shaders/ground.fs", GL_FRAGMENT_SHADER);
 
 	if (!m_vsShader.hasLoaded() || !m_fsShader.hasLoaded()) {
 		closeWindow(true);
@@ -104,7 +104,7 @@ void GameScene::initScene() {
 	glEnable(GL_DEPTH_TEST);
 	glClearDepth(1.0);
 
-	m_snowTexture.loadTexture2D("../res/img/snow.png", true);
+	m_snowTexture.loadTexture2D("res/img/snow.png", true);
 
 	m_sampler.create(true);
 	m_sampler.bind();
