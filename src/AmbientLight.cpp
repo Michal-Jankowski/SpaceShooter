5c0ae802ff8929b4962d6d7e489bbc5285b7d7fa
Michal Jankowski <michaljankowskireal@gmail.com>
2021-11-01 21:34:58 +0100

    Add model class for future lightning

---
+++ src/AmbientLight.cpp
@@ -0,0 +1,20 @@
+#include "AmbientLight.h"
+
+AmbientLight::AmbientLight(const glm::vec3& color, const bool isOn)
+    : m_color(color)
+    , m_isOn(isOn)
+{
+}
+
+std::string AmbientLight::constructAttributeName(const std::string& uniformName, const std::string& attributeName) {
+    return uniformName + "." + attributeName;
+}
+
+void AmbientLight::setUniform(ShaderProgram& shaderProgram, const std::string& uniformName) {
+    shaderProgram.setUniform(constructAttributeName(uniformName, "color"), m_color);
+    shaderProgram.setUniform(constructAttributeName(uniformName, "isOn"), m_isOn);
+}
+
+glm::vec3 AmbientLight::getColorContribution() const {
+    return m_isOn ? m_color : glm::vec3(0.0f);
+}
