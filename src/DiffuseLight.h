195c63ef7b365bc4767533f80b4b2899aaf48b39
Michal Jankowski <michaljankowskireal@gmail.com>
2021-11-01 21:34:58 +0100

    Add model class for future lightning

---
+++ src/DiffuseLight.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "ShaderProgram.h"
+#include <glm/glm.hpp>
+
+class DiffuseLight
+{
+    DiffuseLight(const glm::vec3& color, const glm::vec3& direction, const float factor, const bool isOn = true);
+    void setUniform(ShaderProgram& shaderProgram, const std::string& uniformName);
+    static const DiffuseLight& none();
+private:
+    std::string constructAttributeName(const std::string& uniformName, const std::string& attributeName);
+    glm::vec3 m_color; 
+    glm::vec3 m_direction; 
+    float m_factor; 
+    bool m_isOn;
+};
+
