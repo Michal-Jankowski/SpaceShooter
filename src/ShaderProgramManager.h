39d284870cfcb1ea0f57d4343218ac507cb9bd18
Michal Jankowski <michaljankowskireal@gmail.com>
2021-11-02 13:25:41 +0100

    Add shaderprograManager

---
+++ src/ShaderProgramManager.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "ShaderProgram.h"
+#include <map>
+
+class ShaderProgramManager
+{
+public:
+	static ShaderProgramManager& getInstance();
+
+	ShaderProgram& createShaderProgram(const std::string& key);
+	ShaderProgram& getShaderProgram(const std::string& key) const;
+	void linkAllPrograms();
+	void clearShaderProgramCache();
+private:
+	ShaderProgramManager() {} 
+	ShaderProgramManager(const ShaderProgramManager&) = delete; 
+	void operator=(const ShaderProgramManager&) = delete; 
+
+
+	bool containsShaderProgram(const std::string& key) const;
+
+	std::map<std::string, std::unique_ptr<ShaderProgram>> m_shaderProgramKeys; 
+
+};
+
