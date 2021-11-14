db5c24122df950e8c7f6d721be3fa955f3f56210
Michal Jankowski <michaljankowskireal@gmail.com>
2021-11-14 19:27:49 +0100

    Modify TextureLoader to fix wrong skybox loaded texture

--- src/TextureLoader.h
+++ src/TextureLoader.h
@@ -6,14 +6,17 @@ class TextureLoader
 public:
     ~TextureLoader();
     bool loadTexture2D(const std::string& filePath, bool shouldGenerateMipmaps = false);
-	bool createFromData(const unsigned char* data, int width, int height, int bytesPerPixel, bool generateMipmaps = false);
+	bool createFromData(const unsigned char* data, GLsizei width, GLsizei height, GLenum format, bool generateMipmaps = false);
     void bind(const int textureUnit = 0) const;
+    GLuint getID() const;
+    GLenum getFormat() const;
+    bool resize(GLsizei width, GLsizei height);
     void deleteTexture();
 private:
     GLuint m_textureID = 0; 
-    int m_width = 0; 
-    int m_height = 0; 
-    int m_bytesPerPixel = 0; 
+    GLenum m_format = 0;
+    GLsizei m_width = 0;
+    GLsizei m_height = 0;
     bool m_isLoaded = false; 
     std::string m_filePath; 
 };
