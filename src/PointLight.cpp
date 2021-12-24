baefdfb66cfed51e37f8cdf4abef1bb47186a9e3
Michal Jankowski <michaljankowskireal@gmail.com>
2021-12-24 16:00:39 +0100

    Add pointlight shader struct

---
+++ src/PointLight.cpp
@@ -0,0 +1,48 @@
+#include "PointLight.h"
+
+PointLight::PointLight(const glm::vec3& position, const glm::vec3& color, const float ambientFactor,
+    const float constantAttenuation, const float linearAttenuation, const float exponentialAttenuation,
+    const bool isOn = true)
+    : position(position)
+    , color(color)
+    , ambientFactor(ambientFactor)
+    , constantAttenuation(constantAttenuation)
+    , linearAttenuation(linearAttenuation)
+    , exponentialAttenuation(exponentialAttenuation)
+    , isOn(isOn ? 1 : 0)
+{
+}
+
+std::string PointLight::constructAttributeName(const std::string& uniformName, const std::string& attributeName)
+{
+    return uniformName + "." + attributeName;
+}
+
+void PointLight::setUniform(ShaderProgram& shaderProgram, const std::string& uniformName)
+{
+    shaderProgram.setUniform(constructAttributeName(uniformName, "position"), position);
+    shaderProgram.setUniform(constructAttributeName(uniformName, "color"), color);
+    shaderProgram.setUniform(constructAttributeName(uniformName, "ambientFactor"), ambientFactor);
+    shaderProgram.setUniform(constructAttributeName(uniformName, "constantAttenuation"), constantAttenuation);
+    shaderProgram.setUniform(constructAttributeName(uniformName, "constantAttenuation"), constantAttenuation);
+    shaderProgram.setUniform(constructAttributeName(uniformName, "exponentialAttenuation"), exponentialAttenuation);
+    shaderProgram.setUniform(constructAttributeName(uniformName, "isOn"), isOn);
+}
+
+void* PointLight::getDataPointer() const {
+    return (void*)&position;
+}
+
+GLsizeiptr PointLight::getDataSizeStd140() {
+    // Here it's three times size of vec4 because:
+    // - position + dummy padding make first vec4
+    // - color + ambient factor make second vec4
+    // - attenuation factors + isOn make third vec4
+    return sizeof(glm::vec4) * 3;
+}
+
+
+const PointLight& PointLight::none() {
+    static PointLight nonePointLight(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 0.0f), 0.0f, 0.0f, 0.0f, false);
+    return nonePointLight;
+}
---
+++ src/PointLight.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <GLAD/glad.h>
+#include <glm/common.hpp>
+#include "ShaderProgram.h"
+
+
+class PointLight {
+
+public:
+    PointLight(const glm::vec3& position, const glm::vec3& color, const float ambientFactor,
+        const float constantAttenuation, const float linearAttenuation, const float exponentialAttenuation,
+        const bool isOn = true);
+
+    /**
+     * Sets point light in a shader program.
+     *
+     * @param shaderProgram  Shader program to set point light in
+     * @param uniformName    Name of the uniform variable
+     */
+    void setUniform(ShaderProgram& shaderProgram, const std::string& uniformName);
+
+    /**
+     * Gets data size of the structure (in bytes) according to std140 layout rules.
+     */
+    static GLsizeiptr getDataSizeStd140();
+    void* getDataPointer() const;
+
+    /**
+     * Returns a point light that is off and does not contribute at all.
+     */
+private:
+    std::string constructAttributeName(const std::string& uniformName, const std::string& attributeName);
+    static const PointLight& none();
+
+    glm::vec3 position; // Position of the point light
+    float __DUMMY_PADDING0__; // This is just needed because of std140 layout padding rules
+
+    glm::vec3 color; // Color of the point light
+    float ambientFactor; // Ambient factor (how much this light contributes to the global lighting in any case)
+
+    float constantAttenuation; // Constant attenuation factor of light with rising distance
+    float linearAttenuation; // Lienar attenuation factor of light with rising distance
+    float exponentialAttenuation; // Constant attenuation factor of light with rising distance
+    GLint isOn; // Flag telling, if the light is on
+};
