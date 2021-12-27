6d6a138b9a6767888a7f6f0ad23ca5b6cfe9a160
rainsha <aklinaoo@gmail.com>
2021-12-27 11:23:48 +0100

    Merge hotfix

--- src/engine/buffers/Line.h
+++ src/engine/buffers/Line.h
@@ -7,8 +7,8 @@ public:
 	Line(glm::vec3 start, glm::vec3 end);
 	~Line();
 	void draw();
-	void setColor(glm::vec4 color);
-	void setPosition(glm::vec3 start, glm::vec3 end);
+	void setColor(const glm::vec4& color);
+	void setPosition(const glm::vec3& start, const glm::vec3& end);
     [[nodiscard]] glm::vec3 getStartPosition() const;
     [[nodiscard]] glm::vec3 getEndPosition() const;
 private:
